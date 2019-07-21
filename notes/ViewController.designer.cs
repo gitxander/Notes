277666fac10694c12eb9d80328d5218ce0da253d
Xander Lopez <xanderlopez@me.com>
2019-07-21 20:34:29 +1000

    Add button and toggle button

--- notes/ViewController.designer.cs
+++ notes/ViewController.designer.cs
@@ -11,8 +11,19 @@ using UIKit;
 
 namespace notes
 {
-    [Register("ViewController")]
+    [Register ("ViewController")]
     partial class ViewController
     {
+        [Action ("UIButton393_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void UIButton393_TouchUpInside (UIKit.UIButton sender);
+
+        [Action ("UIButton401_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void UIButton401_TouchUpInside (UIKit.UIButton sender);
+
+        void ReleaseDesignerOutlets ()
+        {
+        }
     }
 }
