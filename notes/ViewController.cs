60b3d3125c71d8bb7b62834df274c2210c222e82
Xander Lopez <xanderlopez@me.com>
2019-07-25 20:43:52 +1000

    Dynamic positioning of newly added text

--- notes/ViewController.cs
+++ notes/ViewController.cs
@@ -10,7 +10,7 @@ namespace notes
         {
         }
 
-        private int a = 20;
+        private int a = 125;
 
         public override void ViewDidLoad()
         {
@@ -23,14 +23,14 @@ namespace notes
 
                 UILabel label = new UILabel()
                 {
-                    Frame = new CoreGraphics.CGRect(a, 124, View.Bounds.Width - 40, 40),
+                    Frame = new CoreGraphics.CGRect(20, a, View.Bounds.Width - 40, 40),
                     Text = textField.Text,
                     Font = UIFont.FromName("Papyrus", 20f),
                     TextColor = UIColor.Magenta,
                     TextAlignment = UITextAlignment.Center
                 };
 
-                a += 20;
+                a += 50;
 
               
                 View.AddSubview(label);
